fe52f93fcc80663f7e3a77883d8298c7f2b4c3ca
moonti <tmmoond8@gmail.com>
2019-04-07 15:19:35 +0900

    #1.5 GraphQL Yoga and Express part 2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('work!!');
+import { Options } from 'graphql-yoga';
+import app from './app';
+
+const PORT : number | string = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT : string = "/playground";
+const GRAPHQL_ENDPOINT : string = "/graphql";
+
+const appOptions : Options = {
+  port: PORT,
+  playground: PLAYGROUND_ENDPOINT,
+  endpoint: GRAPHQL_ENDPOINT
+}
+
+const handleAppStat = () => console.log(`Listening on port ${PORT}`)
+app.start(appOptions, handleAppStat);
