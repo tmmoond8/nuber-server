0a946752c5513d726111e58b5b3b286a158135cc
moonti <tmmoond8@gmail.com>
2019-04-19 14:12:04 +0900

    #1.40 EmailSignUp Resolver

---
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -0,0 +1,40 @@
+import { Resolvers } from "src/types/resolvers";
+import { EmailSignUpMutationArgs, EmailSignUpResponse } from "src/types/graph";
+import User from "../../../entities/User";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EmailSignUp: async (
+      _, 
+      args: EmailSignUpMutationArgs
+    ): Promise<EmailSignUpResponse> => {
+      try {
+        const { email } = args;
+        const existingUser = await User.findOne({ email });
+        if(existingUser) {
+          return {
+            ok: false,
+            error: 'existing email. You should log in instead',
+            token: null
+          }
+        } else {
+          const newUser = await User.create({ ...args });
+          return {
+            ok: true,
+            error: null,
+            token: 'comming soon'
+          }
+        }
+      } catch(error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        }
+      }
+    }
+  }
+
+}
+
+export default resolvers;
