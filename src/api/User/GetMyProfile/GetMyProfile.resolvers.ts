b47f8dd8dfe182a173cbaf5fde25f1df9bbd5359
moonti <tmmoond8@gmail.com>
2019-04-20 17:53:27 +0900

    #1.48 Protecting Resolvers with Middlewares

--- src/api/User/GetMyProfile/GetMyProfile.resolvers.ts
+++ src/api/User/GetMyProfile/GetMyProfile.resolvers.ts
@@ -1,15 +1,16 @@
 import { Resolvers } from "src/types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
 
 const resolvers: Resolvers = {
   Query : {
-    GetMyProfile: async (_, __, context) => {
+    GetMyProfile: privateResolver(async (_, __, context) => {
       const { req: { user } } = context;
       return {
         ok: true,
         error: null,
         user
       }
-    }
+    })
   }
 }
 
