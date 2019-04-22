6f8819d01e14901c08f257ee0fe31de61339ef4b
moonti <tmmoond8@gmail.com>
2019-04-22 17:56:25 +0900

    #1.53 RequestEmailVerification Resolver

---
+++ src/api/User/RequestEmailVerification/RequestEmailVerification.resolvers.ts
@@ -0,0 +1,46 @@
+import { Resolvers } from "src/types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import User from "../../../entities/User";
+import Verification from "../../../entities/Verification";
+import { sendVerificationEmail } from "../../../utils/sendEmail";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    RequestEmailVerification: privateResolver(async (_, __, { req }) => {
+      const user: User = req.user;
+      if (user.email) {
+        try {
+          const oldVerification = await Verification.findOne({
+            payload: user.email
+          });
+          if(oldVerification) {
+            oldVerification.remove();
+          }
+          const newVerification: Verification = await Verification.create({
+            payload: user.email,
+            target: "EMAIL"
+          }).save();
+          if(newVerification) {
+            await sendVerificationEmail(user.fullName, newVerification.key);
+          }
+          return {
+            ok: true,
+            error: null
+          };
+        } catch(error) {
+          return {
+            ok: false,
+            error: error.message
+          }
+        }
+      } else {
+        return {
+          ok: false,
+          error: "not found the email to verify"
+        };
+      }
+    })
+  }
+};
+
+export default resolvers;
