fac27b2a0586e8eeacb0fb4681f7ba6071bbad9f
moonti <tmmoond8@gmail.com>
2019-04-23 10:56:25 +0900

    #1.59 ReportMovement Resolver

--- src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -1,34 +1,34 @@
 import { Resolvers } from "src/types/resolvers";
+import { ReportMovementMutationArgs, ReportMovementResponse } from 'src/types/graph';
 import cleanNullArgs from "../../../utils/cleanNullArgs";
 import privateResolver from "../../../utils/privateResolver";
 import User from "../../../entities/User";
 
 const resolvers: Resolvers = {
   Mutation: {
-    UpdateMyProfile: privateResolver(async (_, args, { req }) => {
+    ReportMovement: privateResolver(
+      async (
+        _,
+        args: ReportMovementMutationArgs,
+        { req }
+      ): Promise<ReportMovementResponse> => {
         const user: User = req.user;
-        const notNull: object = cleanNullArgs(args);
-        if(notNull.hasOwnProperty('password')) {
-          user.password = notNull['password'];
-          user.save();
-          delete notNull['password'];
-        }
-
+        const notNull = cleanNullArgs(args);
         try {
           await User.update({ id: user.id }, { ...notNull });
           return {
             ok: true,
             error: null
           }
-        } catch (error) {
+        } catch(error) {
           return {
             ok: false,
             error: error.message
-          }
+          };
         }
       }
     )
-  }
-};
+  } 
+}
 
 export default resolvers;
